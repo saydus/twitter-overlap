f51ac6bdba46f4a662c0e86a35d1ab1f83dcdbff
saydus <saydus07@gmail.com>
2020-09-10 06:04:30 -0500

    Fetched 100k followers of second user

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# twitter-overlap
-A bot to find the number of overlapping followers between different Twitter users. 
+Twitter Overlap
+---
+A python bot to find the number of overlapping followers between different Twitter users. 
+ 
+The script uses [tweepy](https://github.com/tweepy/tweepy). Install it by running
+`pip install tweepy`.
+
+The `config.yaml` should have authentication keys for your Twitter Developer application in the following format:
+```yaml
+consumer_key: "your_consumer_key"
+consumer_secret: "your_consumer_secret"
+access_token: "your_access_token"
+access_token_secret: "your_access_token_secret"
+
+first_user: "username1"
+second_user: "username2"
+```
---
+++ scraper.py
@@ -0,0 +1,46 @@
+import requests
+import os
+import json
+
+def auth():
+    return os.environ.get("BEARER_TOKEN")
+
+def create_url():
+    query = "from:twitterdev -is:retweet"
+    # Tweet fields are adjustable.
+    # Options include:
+    # attachments, author_id, context_annotations,
+    # conversation_id, created_at, entities, geo, id,
+    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+    # source, text, and withheld
+    tweet_fields = "tweet.fields=author_id"
+    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
+        query, tweet_fields
+    )
+    return url
+
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+
+def connect_to_endpoint(url, headers):
+    response = requests.request("GET", url, headers=headers)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+def main():
+    bearer_token = auth()
+    url = create_url()
+    headers = create_headers(bearer_token)
+    json_response = connect_to_endpoint(url, headers)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
