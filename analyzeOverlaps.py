6eb0af2cae6ce93e3c988cd4d21259bac4bf9ac6
saydus <saydus07@gmail.com>
2020-09-27 23:55:59 -0500

    Generalized mongo field names

--- analyzeOverlaps.py
+++ analyzeOverlaps.py
@@ -29,33 +29,32 @@ followers_collection = db[second_user_handle]
 print(api.rate_limit_status()) # print info about the rate limits for Tweepy
 print(followers_collection) # print info about the collection
 
-print("Users checked: " + str(followers_collection.count_documents({"checked": True})))
-print("Overlaps found: " + str(followers_collection.count_documents({"followsOtherUser": True})))
+print("Users checked: " + str(followers_collection.count_documents({first_user_handle + 'Checked': True})))
+print("Overlaps found: " + str(followers_collection.count_documents({first_user_handle: True})))
 
 # Iterate through every follower of second user found and see if they follow first used
 users_checked = 0  # how many users we will iterate on
 num_of_fails = 0 # number of times tweepy fails when fetching followers
 
-for document in followers_collection.find({"checked": False}):
+
+for document in followers_collection.find():
     # see if they are subscribed to user 1
     users_checked += 1
 
     try:
         if api.show_friendship(source_id=document["name"], target_screen_name=first_user_handle)[0].following:
-            followers_collection.find_one_and_update({"_id": document["_id"]}, {"$set": {"followsOtherUser": True}})
+            followers_collection.find_one_and_update({"_id": document["_id"]}, {"$set": {first_user_handle: True}})
     except tweepy.error.TweepError as err:
         print("Failed at some point with tweepy, will just skip this follower: {0}".format(err))
         num_of_fails += 1
         followers_collection.find_one_and_update({"_id": document["_id"]}, {"$set": {"failed": True}})
 
-    followers_collection.find_one_and_update({"_id": document["_id"]}, {"$set": {"checked": True}})
+    followers_collection.find_one_and_update({"_id": document["_id"]}, {"$set": {first_user_handle + 'Checked': True}})
 
     # Cooldown for 15 mins
     if users_checked % 180 == 0:
         print("Sleeping for 15 minutes now")
         time.sleep(15 * 60)
-
-
 
 
 # Statements in console
@@ -64,4 +63,3 @@ num_overlap = followers_collection.count_documents({"followsOtherUser": True})
 print("Tweepy failed ", num_of_fails, " times")
 print("Users checked: ", num_checked)
 print("Overlaps found: ", num_overlap)
-print("Percentage of overlaps: ", num_overlap/num_overlap*100)
